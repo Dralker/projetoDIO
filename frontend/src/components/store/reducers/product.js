a481557e5d82c4da3674fbb408774e874ecfacf2
Dralker <86241975+Dralker@users.noreply.github.com>
2022-02-02 13:12:15 -0300

    first commit

---
+++ frontend/src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Ação",
+      fk_idcategorys: 1,
+      name_product: "Horizon ",
+      price: 120.90,
+      image: "./images/produtos/d1.jpeg",
+      id_categorys: 1,
+      name_categorys: "Mundo Aberto",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "Ação",
+      fk_idcategorys: 1,
+      name_product: "Batman",
+      price: 220.90,
+      image: "./images/produtos/d2.jpeg",
+      id_categorys: 1,
+      name_categorys: "Mundo Aberto",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "RPG",
+      fk_idcategorys: 1,
+      name_product: "Final Fantasy 7",
+      price: 230.50,
+      image: "./images/produtos/d3.jpeg",
+      id_categorys: 1,
+      name_categorys: "Estrategia",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "Aventura",
+      fk_idcategorys: 2,
+      name_product: "Jumanji",
+      price: 99.99,
+      image: "./images/produtos/d4.jpg",
+      id_categorys: 2,
+      name_categorys: "Aventura",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Corrida",
+      fk_idcategorys: 2,
+      name_product: "Carros",
+      price: 350.99,
+      image: "./images/produtos/d5.jpg",
+      id_categorys: 2,
+      name_categorys: "Corrida",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "Ação",
+      fk_idcategorys: 2,
+      name_product: "GTA 5",
+      price: 299.99,
+      image: "./images/produtos/d6.jpeg",
+      id_categorys: 2,
+      name_categorys: "Mundo Aberto",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Aventura",
+      fk_idcategorys: 3,
+      name_product: "Uncharted 4",
+      price: 150.50,
+      image: "./images/produtos/d7.jpeg",
+      id_categorys: 3,
+      name_categorys: "Aventura",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Suspense",
+      fk_idcategorys: 3,
+      name_product: "Hello Neighbor",
+      price: 100.90,
+      image: "./images/produtos/d8.jpg",
+      id_categorys: 3,
+      name_categorys: "Suspense",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Ação",
+      fk_idcategorys: 4,
+      name_product: "God of War",
+      price: 350,
+      image: "./images/produtos/d9.png",
+      id_categorys: 4,
+      name_categorys: "Açao",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Suspense",
+      fk_idcategorys: 4,
+      name_product: "The Last of Us 2",
+      price: 399,
+      image: "./images/produtos/d10.jpeg",
+      id_categorys: 4,
+      name_categorys: "Suspense",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Ação",
+      fk_idcategorys: 5,
+      name_product: "Ghost of Tsushima",
+      price: 180,
+      image: "./images/produtos/d11.jpg",
+      id_categorys: 5,
+      name_categorys: "Mundo Aberto",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "Estrategia",
+      fk_idcategorys: 5,
+      name_product: "Assasin's Creed",
+      price: 189.99,
+      image: "./images/produtos/d12.jpeg",
+      id_categorys: 5,
+      name_categorys: "Mundo Aberto",
+      selected: false
+    },
+];
+
+export default productsList;
